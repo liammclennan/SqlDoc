db218bd71bdef890b71ee38ae6cf349b048d6604
Liam McLennan <liam.mclennan@readify.net>
2014-09-25 16:00:59 +1000

    Upgrade to VS2013

---
+++ PostgresDoc/AssemblyInfo.fs
@@ -0,0 +1,10 @@
+﻿namespace PostgresDoc
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+()
