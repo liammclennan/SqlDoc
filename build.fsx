a3cb96fa43528bbfdfc0a7ed5afb5b106765f26b
Liam McLennan <liam.mclennan@gmail.com>
2015-11-30 10:31:47 +1000

    playing with build.fsx

--- build.fsx
+++ build.fsx
@@ -1,11 +1,15 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
+open Fake.Testing
 open System.IO
 
 Target "Build" <| fun _ ->
     let projects =
         [
             "SqlDoc/SqlDoc.fsproj"
+            "Tests/Tests.fsproj"
+            "SqlDocCs/SqlDocCs.csproj"
+            "TestsCs/TestsCs.csproj"
         ]
     for projFile in projects do
         build (fun x ->
@@ -22,13 +26,13 @@ Target "Default" (fun _ ->
     ()
 )
 
-//Target "Test" (fun _ ->
-//    !! (testDir + "/Tests.dll")
-//      |> NUnit (fun p ->
-//          {p with
-//             DisableShadowCopy = true;
-//             OutputFile = testDir + "TestResults.xml" })
-//)
+Target "Test" (fun _ ->
+    let testDir = "Tests/"
+    !! (testDir + "bin/Release/Tests.dll")
+      |> xUnit (fun p -> {p with HtmlOutputPath = testDir @@ "html" |> Some; ToolPath = "packages/xunit.runner.console/tools/xunit.console.exe";NUnitXmlOutputPath = Some testDir}))
+
+"Build"
+    ==> "Test"
 
 // start build
 RunTargetOrDefault "Build"
